431360a9ab3603124074f368c1b192791e7b32af
besumero628 <besumero628@gmail.com>
2022-02-26 23:02:43 +0900

    12-6 routeの設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import { ChakraProvider, Button } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
+
 import theme from './theme/theme';
+import { Router } from './router/Router';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Button colorScheme="teal">button</Button>
-      <p>aaaa</p>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
