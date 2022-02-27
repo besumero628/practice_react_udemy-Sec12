3edbd408e1530cb1c74861e48d25b5720b982c89
besumero628 <besumero628@gmail.com>
2022-02-27 22:11:12 +0900

    12-9 Header Routeing

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,14 +1,23 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Box, Flex, Heading, Link, useDisclosure } from "@chakra-ui/react";
-import { memo, VFC } from "react";
+import { on } from "events";
+import { memo, useCallback, VFC } from "react";
+import { useHistory } from "react-router-dom";
 import { MenuIconButton } from "../../atoms/button/MenuiconButton";
 import { MenuDrawer } from "../../molecules/MenuDrawer";
 
 export const Header: VFC = memo(() => {
   const {isOpen, onOpen, onClose} = useDisclosure()
+  const history = useHistory()
+
+  const onClickHome = useCallback(()=> history.push("/home"), [])
+  const onClickUserManagement = useCallback(()=> history.push("/home/user_management"), [])
+  const onClickSetting = useCallback(()=> history.push("/home/setting"), [])
+
   return (
     <>
       <Flex as="nav" bg="teal.500" color="gray.50" align="center" justify="space-between" padding={{base: 3, md: 5}}>
-        <Flex align="center" as="a" mr={8} _hover={{cursor: "pointer"}}>
+        <Flex align="center" as="a" mr={8} _hover={{cursor: "pointer"}} onClick={onClickHome}>
           <Heading as="h1" fontSize={{base: "md", md:"lg"}}>
             ユーザー管理アプリ
           </Heading>
@@ -16,13 +25,13 @@ export const Header: VFC = memo(() => {
         
         <Flex align="center" fontSize="sm" flexGrow={2} display={{base: "none", md: "flex"}}>
           <Box pr={4}>
-            <Link>ユーザー一覧</Link>
+            <Link onClick={onClickUserManagement}>ユーザー一覧</Link>
           </Box>
-          <Link>設定</Link>
+          <Link onClick={onClickSetting}>設定</Link>
         </Flex>
         <MenuIconButton onOpen={onOpen} />
       </Flex>
-      <MenuDrawer onClose={onClose} isOpen={isOpen} />
+      <MenuDrawer onClose={onClose} isOpen={isOpen} onClickHome={onClickHome} onClickUserManagement={onClickUserManagement} onClickSetting={onClickSetting} />
     </>
   )
 })
