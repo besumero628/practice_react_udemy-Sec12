74bc4eb001c41e7b376245b07599cfa71a422328
besumero628 <besumero628@gmail.com>
2022-03-01 23:41:02 +0900

    Add user一覧取得機能の実装

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,13 +1,29 @@
-import { Wrap, WrapItem } from "@chakra-ui/react";
-import { memo, VFC } from "react";
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
+import { memo, useEffect, VFC } from "react";
+import { useAllUsers } from "../../hooks/useAllUsers";
 import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
+  const { getUsers, loading, users } = useAllUsers()
+
+  useEffect(()=> getUsers(), [])
+
   return (
-    <Wrap p={{base: 4, md: 10}}>
-      <WrapItem>
-        <UserCard imageUrl="https://source.unsplash.com/random" userName="besumero628" fullName="Nara syuhei" />
-      </WrapItem>
-    </Wrap>
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{base: 4, md: 10}} justify="center">
+          {users.map((user)=> (
+            <WrapItem key={user.id}>
+              <UserCard imageUrl="https://source.unsplash.com/random" userName={user.username} fullName={user.name} />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
+    
   )
 })
