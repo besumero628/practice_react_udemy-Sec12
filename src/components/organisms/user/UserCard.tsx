fc91c40aafa302e3da89fba53db97ee47f977d4e
besumero628 <besumero628@gmail.com>
2022-03-02 21:38:31 +0900

    Add user detail modal

--- src/components/organisms/user/UserCard.tsx
+++ src/components/organisms/user/UserCard.tsx
@@ -5,12 +5,13 @@ type Props = {
   imageUrl: string;
   userName: string;
   fullName: string;
+  onClick: () => void;
 }
 
 export const UserCard: VFC<Props> = memo((props) => {
-  const { imageUrl, userName, fullName } = props
+  const { imageUrl, userName, fullName, onClick } = props
   return (
-    <Box p={4} w="260px" h="260px" bg="white" borderRadius="10px" shadow="md" _hover={{cursor: "pointer", opacity: 0.8}}>
+    <Box p={4} w="260px" h="260px" bg="white" borderRadius="10px" shadow="md" _hover={{cursor: "pointer", opacity: 0.8}} onClick={onClick}>
       <Stack textAlign="center">
         <Image borderRadius="full" boxSize="160px" src={imageUrl} alt={userName} m="auto" />
         <Text fontSize="lg" fontWeight="bold">{userName}</Text>
--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,12 +1,16 @@
-import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
-import { memo, useEffect, VFC } from "react";
+import { Center, Spinner, useDisclosure, Wrap, WrapItem } from "@chakra-ui/react";
+import { memo, useCallback, useEffect, VFC } from "react";
 import { useAllUsers } from "../../hooks/useAllUsers";
 import { UserCard } from "../organisms/user/UserCard";
+import { UserDetailModal } from "../organisms/user/UserDetailModal";
 
 export const UserManagement: VFC = memo(() => {
+  const { isOpen, onOpen, onClose  } = useDisclosure()
   const { getUsers, loading, users } = useAllUsers()
 
   useEffect(()=> getUsers(), [])
+
+  const onClickUser = useCallback(() => onOpen(), [])
 
   return (
     <>
@@ -18,11 +22,12 @@ export const UserManagement: VFC = memo(() => {
         <Wrap p={{base: 4, md: 10}} justify="center">
           {users.map((user)=> (
             <WrapItem key={user.id}>
-              <UserCard imageUrl="https://source.unsplash.com/random" userName={user.username} fullName={user.name} />
+              <UserCard imageUrl="https://source.unsplash.com/random" userName={user.username} fullName={user.name} onClick={onClickUser} />
             </WrapItem>
           ))}
         </Wrap>
       )}
+      <UserDetailModal isOpen={isOpen} onClose={onClose} />
     </>
     
   )
